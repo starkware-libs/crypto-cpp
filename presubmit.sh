d3264b842f10b0fc442271378b482ae5e704b3e1
Jakub Ptak <jakub.ptak@swmansion.com>
2021-12-20 09:48:03 +0100

    Make compilation possible on macos

--- presubmit.sh
+++ presubmit.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-(cd src/starkware/crypto/ffi/js; npm install)
+(cd src/starkware/crypto/ffi/js; npm install --no-package-lock)
 
 mkdir -p build/Release
 (cd build/Release; cmake -DCMAKE_BUILD_TYPE=Release ../..)
 make -C build/Release
 CTEST_OUTPUT_ON_FAILURE=1 make -C build/Release test
-clang-tidy-6.0 -header-filter=src/starkware -p=build/Release $(find src/starkware -name "*.cc" | grep -v node_modules)
+clang-tidy -header-filter=src/starkware -p=build/Release $(find src/starkware -name "*.cc" | grep -v node_modules)
 cpplint --extensions=cc,h $(find src/starkware | grep -v node_modules | grep -E '\.(cc|h)$')

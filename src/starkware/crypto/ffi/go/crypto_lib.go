c64bf890d3d7097ad0c2ccd31b06db46cd1abb33
Ömer Faruk IRMAK <omerfirmak@gmail.com>
2022-11-10 18:44:12 +0300

    Export Binary equivalents of Go bindings

--- src/starkware/crypto/ffi/go/crypto_lib.go
+++ src/starkware/crypto/ffi/go/crypto_lib.go
@@ -4,6 +4,7 @@ import "encoding/hex"
 import (
 	"fmt"
 	"math/big"
+	"errors"
 )
 /*
 
@@ -18,7 +19,7 @@ import "C"
 import "unsafe"
 
 
-const curveOrder = "800000000000010ffffffffffffffffb781126dcae7b2321e66a241adc64d2f"
+var curveOrder, _ = new(big.Int).SetString("800000000000010ffffffffffffffffb781126dcae7b2321e66a241adc64d2f", 16)
 
 /*
   Computes the component s of a signature given the component w.
@@ -26,11 +27,15 @@ const curveOrder = "800000000000010ffffffffffffffffb781126dcae7b2321e66a241adc64
 func invertOnCurve(w string) string {
 	w_big_int := new(big.Int)
 	w_big_int.SetString(w[2:], 16)
-	order := new(big.Int)
-	order.SetString(curveOrder, 16)
-	w_big_int.ModInverse(w_big_int, order)
+	w_big_int.ModInverse(w_big_int, curveOrder)
 	s := fmt.Sprintf("0x0%x", w_big_int)
 	return s
+}
+
+func invertOnCurveBinary(w []byte) []byte {
+	w_big_int := new(big.Int).SetBytes(w)
+	w_big_int.ModInverse(w_big_int, curveOrder)
+	return w_big_int.Bytes()
 }
 
 /*
@@ -63,16 +68,24 @@ func Hash(input1, input2 string) string {
 	input2_dec, _ := hex.DecodeString(padHexString(input2))
 	var out [1024]byte
 
+	if err := HashBinary(input1_dec, input2_dec, out[:]); err != nil {
+		return ""
+	}
+
+	return "0x" + hex.EncodeToString(out[:32])
+}
+
+func HashBinary(input1, input2, out []byte) error {
 	res := C.Hash(
-		(*C.char)(unsafe.Pointer(&input1_dec[0])),
-		(*C.char)(unsafe.Pointer(&input2_dec[0])),
+		(*C.char)(unsafe.Pointer(&input1[0])),
+		(*C.char)(unsafe.Pointer(&input2[0])),
 		(*C.char)(unsafe.Pointer(&out[0])))
 
 	if res != 0 {
-		fmt.Printf("Pedersen hash encountered an error: %s\n", out)
-		return ""
+		return errors.New("Pedersen hash encountered an error")
 	}
-	return "0x" + hex.EncodeToString(out[:32])
+
+	return nil
 }
 
 /*
@@ -82,15 +95,23 @@ func GetPublicKey(private_key string) string {
 	private_key_dec, _ := hex.DecodeString(padHexString(private_key))
 	var out [1024]byte
 
-	res := C.GetPublicKey((*C.char)(unsafe.Pointer(&private_key_dec[0])),
-		(*C.char)(unsafe.Pointer(&out[0])))
-
-	if res != 0 {
-		fmt.Printf("GetPublicKey encountered an error: %s\n", C.GoBytes(unsafe.Pointer(&out[0]), 1024))
+	if err := GetPublicKeyBinary(private_key_dec, out[:]); err != nil {
 		return ""
 	}
 
 	return "0x" + hex.EncodeToString(out[:32])
+}
+
+func GetPublicKeyBinary(private_key, out []byte) error {
+	res := C.GetPublicKey(
+		(*C.char)(unsafe.Pointer(&private_key[0])),
+		(*C.char)(unsafe.Pointer(&out[0])))
+
+	if res != 0 {
+		return errors.New("GetPublicKey encountered an error")
+	}
+
+	return nil
 }
 
 /*
@@ -102,20 +123,20 @@ func Verify(stark_key, msg_hash, r, s string) bool {
 	stark_key_dec, _ := hex.DecodeString(padHexString(stark_key))
 	message_dec, _ := hex.DecodeString(padHexString(msg_hash))
 	r_dec, _ := hex.DecodeString(padHexString(r))
+	s_dec, _ := hex.DecodeString(padHexString(s))
 
-	w := invertOnCurve(s)
-	w_dec, _ := hex.DecodeString(padHexString(w))
+	return VerifyBinary(stark_key_dec, message_dec, r_dec, s_dec)
+}
+
+func VerifyBinary(stark_key, msg_hash, r, s []byte) bool {
+	w := invertOnCurveBinary(s)
 
 	res := C.Verify(
-		(*C.char)(unsafe.Pointer(&stark_key_dec[0])),
-		(*C.char)(unsafe.Pointer(&message_dec[0])),
-		(*C.char)(unsafe.Pointer(&r_dec[0])),
-		(*C.char)(unsafe.Pointer(&w_dec[0])))
-
-	if res == 0 {
-		return false
-	}
-	return true
+		(*C.char)(unsafe.Pointer(&stark_key[0])),
+		(*C.char)(unsafe.Pointer(&msg_hash[0])),
+		(*C.char)(unsafe.Pointer(&r[0])),
+		(*C.char)(unsafe.Pointer(&w[0])))
+	return res != 0;
 }
 
 /*
@@ -130,21 +151,28 @@ func Sign(private_key, message, k string) (string, string) {
 	k_dec, _ := hex.DecodeString(padHexString(k))
 	var out [1024]byte
 
-	ret := C.Sign(
-		(*C.char)(unsafe.Pointer(&private_key_dec[0])),
-		(*C.char)(unsafe.Pointer(&message_dec[0])),
-		(*C.char)(unsafe.Pointer(&k_dec[0])),
-		(*C.char)(unsafe.Pointer(&out[0])))
-
-	if (ret != 0) {
+	if err := SignBinary(private_key_dec,message_dec, k_dec, out[:]); err != nil {
 		return "", ""
 	}
 
 	res := hex.EncodeToString(out[:64])
 	signature_r := "0x" + res[0:64]
-	signature_w := "0x" + res[64:]
-
-	signature_s := invertOnCurve(signature_w)
-
+	signature_s := "0x" + res[64:]
 	return signature_r, signature_s
+}
+
+func SignBinary(private_key, message, k, out []byte) error {
+	ret := C.Sign(
+		(*C.char)(unsafe.Pointer(&private_key[0])),
+		(*C.char)(unsafe.Pointer(&message[0])),
+		(*C.char)(unsafe.Pointer(&k[0])),
+		(*C.char)(unsafe.Pointer(&out[0])))
+
+	copy(out[32:64], invertOnCurveBinary(out[32:64]))
+
+	if (ret != 0) {
+		return errors.New("SignBinary encountered an error")
+	}
+
+	return nil
 }
